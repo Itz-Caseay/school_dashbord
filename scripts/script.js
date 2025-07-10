9c74082e8982d28de5c53d1bee52d3f6a3ad9359
ekumbenita <ekumbenita@gmail.com>
2025-07-10 15:37:05 +0200

    benita manages to push through Caseay

--- scripts/script.js
+++ scripts/script.js
@@ -3,3 +3,19 @@
 function date() {
     alert('Recaptcha not available for the moment');
 }
+
+const menu = document.getElementById('menu');
+const navLinks = document.getElementById('navLinks');
+
+menu.addEventListener('click', () => {
+    navLinks.classList.toggle('left-nav');
+});
+
+document.getElementById('menu').addEventListener('click', function() {
+            document.getElementById('navLinks').style.display = 'block';
+        });
+
+        document.getElementById('navLinks').addEventListener('click', function(){
+            document.getElementById('navLinks').style.display = 'none';
+        });
+        
